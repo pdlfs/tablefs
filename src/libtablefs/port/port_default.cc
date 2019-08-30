551d3975db69d4de1252e36ef25f6e135801ce94
Qing Zheng <zhengq@cs.cmu.edu>
2019-08-30 19:59:29 -0400

    add port default

---
+++ src/libtablefs/port/port_default.cc
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2019 Carnegie Mellon University,
+ * Copyright (c) 2019 Triad National Security, LLC, as operator of
+ *     Los Alamos National Laboratory.
+ *
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. See the AUTHORS file for names of contributors.
+ */
+
+#include "port_default.h"
+
+namespace pdlfs {
+
+MDBOptions::MDBOptions() : db(NULL) {}
+
+struct MDB::Tx {
+  const Snapshot* snap;
+  WriteBatch bat;
+};
+
+MDB::MDB(const MDBOptions& options) : MXDB(options.db) {}
+
+MDB::~MDB() {}
+
+Status MDB::Get(const DirId& id, const Slice& fname, Stat* stat) {
+  ReadOptions read_opts;
+  Tx* const tx = NULL;
+  return GET<Key>(id, fname, stat, NULL, &read_opts, tx);
+}
+
+Status MDB::Set(const DirId& id, const Slice& fname, const Stat& stat) {
+  WriteOptions write_opts;
+  Tx* const tx = NULL;
+  return SET<Key>(id, fname, stat, fname, &write_opts, tx);
+}
+
+Status MDB::Delete(const DirId& id, const Slice& fname) {
+  WriteOptions write_opts;
+  Tx* const tx = NULL;
+  return DELETE<Key>(id, fname, &write_opts, tx);
+}
+
+MDB::Dir* MDB::Opendir(const DirId& id) {
+  ReadOptions read_opts;
+  Tx* const tx = NULL;
+  return OPENDIR<Iterator, Key>(id, &read_opts, tx);
+}
+
+Status MDB::Readdir(Dir* dir, Stat* stat, std::string* name) {
+  return READDIR<Iterator>(dir, stat, name);
+}
+
+void MDB::Closedir(MDB::Dir* dir) {
+  return CLOSEDIR(dir);  // This also deletes dir
+}
+
+}  // namespace pdlfs
---
+++ src/libtablefs/port/port_default.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2019 Carnegie Mellon University,
+ * Copyright (c) 2019 Triad National Security, LLC, as operator of
+ *     Los Alamos National Laboratory.
+ *
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. See the AUTHORS file for names of contributors.
+ */
+#pragma once
+
+#include "pdlfs-common/mdb.h"
+
+namespace pdlfs {
+
+// Options for controlling MDB behavior.
+struct MDBOptions {
+  MDBOptions();
+  DB* db;
+};
+
+// An MDB implementation that binds to our own DB implementation. Our DB is a
+// modified LevelDB realization of a LSM-Tree.
+class MDB : public MXDB<DB, Slice, Status, kNameInKey> {
+ public:
+  struct Tx;
+  explicit MDB(const MDBOptions& options);
+  ~MDB();
+
+  Status Get(const DirId& id, const Slice& fname, Stat* stat);
+  Status Set(const DirId& id, const Slice& fname, const Stat& stat);
+  Status Delete(const DirId& id, const Slice& fname);
+
+  typedef MXDB::Dir<Iterator> Dir;
+  Dir* Opendir(const DirId& id);
+  Status Readdir(Dir* dir, Stat* stat, std::string* name);
+  void Closedir(Dir* dir);
+
+ private:
+  void operator=(const MDB&);
+  MDB(const MDB&);
+};
+
+}  // namespace pdlfs
